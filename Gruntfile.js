a4839ed52f21acbf1e4ee78075675165c8457a0b
edsilv <e.silverton@gmail.com>
2014-01-18 15:43:20 +0000

    build to examples directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,6 @@ module.exports = function (grunt) {
     grunt.initConfig({
         global:
         {
-            buildDir: './build',
             minify: 'optimize=none'
         },
         pkg: packageJson,
@@ -38,10 +37,10 @@ module.exports = function (grunt) {
         exec: {
             // concatenate and compress with r.js
             tests: {
-                cmd: 'node ./tools/r.js -o baseUrl=examples/tests/ mainConfigFile=examples/tests/tests.js name=tests <%= global.minify %> out=<%= global.buildDir %>/tests.js'
+                cmd: 'node ./tools/r.js -o baseUrl=examples/tests/ mainConfigFile=examples/tests/tests.js name=tests <%= global.minify %> out=examples/tests/build/tests.js'
             },
             natureofcode: {
-                cmd: 'node ./tools/r.js -o baseUrl=examples/natureofcode/ mainConfigFile=examples/natureofcode/natureofcode.js name=natureofcode <%= global.minify %> out=<%= global.buildDir %>/natureofcode.js'
+                cmd: 'node ./tools/r.js -o baseUrl=examples/natureofcode/ mainConfigFile=examples/natureofcode/natureofcode.js name=natureofcode <%= global.minify %> out=examples/natureofcode/build/natureofcode.js'
             }
         }
 
@@ -62,11 +61,6 @@ module.exports = function (grunt) {
     // builds examples/tests/tests.ts and references into a single file.
     grunt.registerTask('tests', '', function() {
 
-        // grunt examples-build --buildDir=myDir
-        // or prepend / to target relative to system root.
-        var buildDir = grunt.option('buildDir');
-        if (buildDir) grunt.config.set('global.buildDir', buildDir);
-
         // grunt examples-build --minify
         var minify = grunt.option('minify');
         if (minify) grunt.config.set('global.minify', '');
@@ -79,11 +73,6 @@ module.exports = function (grunt) {
 
     // builds examples/natureofcode/natureofcode.ts and references into a single file.
     grunt.registerTask('natureofcode', '', function() {
-
-        // grunt examples-build --buildDir=myDir
-        // or prepend / to target relative to system root.
-        var buildDir = grunt.option('buildDir');
-        if (buildDir) grunt.config.set('global.buildDir', buildDir);
 
         // grunt examples-build --minify
         var minify = grunt.option('minify');
