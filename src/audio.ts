f2d409069b6dff3cf3b8890bbf3e213940689145
Luke Phillips <luke@femurdesign.com>
2014-08-11 16:45:51 +0100

    playNote function with defaults parameters

--- src/audio.ts
+++ src/audio.ts
@@ -5,73 +5,60 @@ class SoundGen {
     context: AudioContext;
 
 
-
-
     constructor() {
         this.context = new AudioContext();
-
-
-    }
-
-
-
-    bleep(tone: number) {
-
-        // VIEW TYPESCRIPT OPTIONAL PROPERTIES
-
-        console.log('start bleep');
-
-        var oscillator = this.context.createOscillator();
-        var gainNode = this.context.createGain();
-        var panner = this.context.createPanner();
-
-        oscillator.type = 3;
-        oscillator.frequency.value = tone;
-        gainNode.gain.value = 0.1;
-
-//        panner.setPosition(x, y);
-
-        oscillator.connect(panner);
-        panner.connect(gainNode)
-        gainNode.connect(this.context.destination);
-
-
-        oscillator.start(0);
-        var now = this.context.currentTime;
-        gainNode.gain.cancelScheduledValues( now );
-        gainNode.gain.setValueAtTime(0.1, now);
-        gainNode.gain.linearRampToValueAtTime(0, now+0.29);
-
-        setTimeout(function() {
-            oscillator.disconnect();
-            console.log('disconnect bleep');
-        }, 500);
-
-
 
     }
 
     playNote(options?: any){
 
         //DEFAULTS
-        var newNote = {
-            pitch: 440,
-            envelope: [0, 1, 1, 0.4],
+        var defaultOptions = {
+            frequency: 440,
+            oscType: 4,
+            envelope: [0, 0.5, 0, 0.1],
             volume: 0.2
         };
 
-        if (options.pitch) {
-            newNote.pitch = options.pitch;
+        if (typeof options == 'object') {
+            options = $.extend(defaultOptions, options);
+        } else {
+            options = defaultOptions;
         }
-        if (options.envelope) {
-            newNote.envelope = options.envelope;
-        }
-        if (options.volume) {
-            newNote.volume = options.volume;
-        }
-        console.log(newNote);
-        return newNote;
+
+        var oscillator = this.context.createOscillator();
+        var gainNode = this.context.createGain();
+
+        oscillator.type = options.oscType;
+        oscillator.frequency.value = options.frequency;
+        gainNode.gain.value = options.volume;
+
+        oscillator.connect(gainNode);
+        gainNode.connect(this.context.destination);
+
+        //ASDR
+        var attack = options.envelope[0],
+            sustain = options.envelope[1],
+            decay = options.envelope[2],
+            release = options.envelope[3],
+            now = this.context.currentTime;
+
+        gainNode.gain.cancelScheduledValues( now );
+        gainNode.gain.setValueAtTime(0, now);
+        gainNode.gain.linearRampToValueAtTime(options.volume, now + attack);
+//        gainNode.gain.linearRampToValueAtTime(options.volume/4, now + attack);
+        gainNode.gain.linearRampToValueAtTime(0, now + attack + sustain + decay + release);
+
+
+        oscillator.start(0);
+
+        setTimeout(function() {
+            oscillator.disconnect();
+            console.log('disconnect bleep');
+        }, (attack + sustain + decay + release)* 1500);
+
     }
+
 
 
 }
